a1473ae27963a600ea85b6fb1bf33b146c8d8b6d
precious <ajalaprecious200@gmail.com>
2022-09-13 19:01:03 -0700

    maim

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -14,8 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
 #endif
-
-^C
